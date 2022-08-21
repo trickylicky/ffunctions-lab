1f2ba4c8d706541ae6addc08f6e57b6282c9bf01
Tricky Licky <wangombevictor5@gmail.com>
2022-08-21 22:27:05 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function  distanceFromHqInBlocks(value){
+    if(value > 42) {
+      return value - 42;
+    } else {
+      return 42 - value;
+    }
+}function distanceFromHqInFeet(value){
+    const result = distanceFromHqInBlocks(value);
+   return result * 264;
+}
+function distanceTravelledInFeet(startingPoint, destination){
+    if(startingPoint > destination) {
+      return (startingPoint - destination) * 264;
+   } else {
+      return (destination - startingPoint) * 264;
+   }
+}
+function calculatesFarePrice(startingPoint, destination){
+    const distance = distanceTravelledInFeet(startingPoint, destination);
+   if(distance <= 400) {
+      return 0;
+   } else if(distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+   } else if(distance > 2000 && distance <= 2500) {
+      return 25;
+   } else {
+      return 'cannot travel that far';
+   }
+}
